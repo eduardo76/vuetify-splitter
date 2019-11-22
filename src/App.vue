1cbc9b94ca362fac531b1937b02af870892a4688
Eduardo Tavares <etavaresf@gmail.com>
2019-11-22 17:51:39 -0300

    Commit Final

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,59 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img alt="Vuetify Logo" class="shrink mr-2" contain src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png" transition="scale-transition" width="40"/>
+        <v-img alt="Vuetify Name" class="shrink mt-1 hidden-sm-and-down" contain min-width="100" src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png" width="100"/>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text>
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+
+      <splitter value="256" gutter-size="4" min-width="200">
+
+        <template v-slot:panel1>
+          <v-card flat>
+            <v-tree></v-tree>
+            <v-list></v-list>
+          </v-card>
+        </template>
+        
+        <template v-slot:panel2="{ right }">
+          <v-container id="blah">
+            <h1>Panel2 - {{right}}</h1>
+          </v-container>
+        </template>
+
+      </splitter>
+    </v-content>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-#nav {
-  padding: 30px;
-}
+import Splitter from "@/components/Splitter.vue";
+import VTree from "@/components/VTree.vue";
+import VList from "@/components/VList.vue";
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+export default {
+  name: 'App',
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  components: {
+    Splitter,
+    VTree,
+    VList
+  },
+
+};
+</script>
+
+<style scoped>
+
 </style>
